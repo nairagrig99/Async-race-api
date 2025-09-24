30877e6d1d629a56cb2aed1b97389f50fd98bbe5
nairagrig99 <nairagrig1999@gmail.com>
2025-09-24 14:57:28 +0400

    init:add message for empty cars or winners

--- src/components/Garage/Garage.tsx
+++ src/components/Garage/Garage.tsx
@@ -1,7 +1,7 @@
 import CarModal from "./car-race-panel/CarModal.tsx";
 import CarPanel from "./car-race-panel/CarPanel.tsx";
-import {useDispatch} from "react-redux";
-import type {AppDispatch} from "../../store/store.ts";
+import {useDispatch, useSelector} from "react-redux";
+import type {AppDispatch, RootState} from "../../store/store.ts";
 import {useEffect, useState} from "react";
 import Modal from "../../UI/Modal.tsx";
 import {getWinners} from "../../services/WinnersService.ts";
@@ -10,6 +10,7 @@ import WinnerModal from "../../UI/WinnerModal.tsx";
 export default function Garage() {
     const dispatch = useDispatch<AppDispatch>();
     const [carListRace, setCarListRace] = useState<HTMLDivElement[]>([])
+    const selector = useSelector((state: RootState) => state.carSlice.car);
 
     useEffect(() => {
         dispatch(getWinners())
@@ -19,6 +20,7 @@ export default function Garage() {
             setCarListRace(carList);
         }
     }
+    if (!selector.length) return <h3 className="text-[45px] text-center">No Cars</h3>
 
     return <div>
         <h3 className="text-[45px] mb-3">GARAGE</h3>
